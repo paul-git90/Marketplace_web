a2b78e96d8032f0b43cfa02a8fefbb36c1467a61
paul-git90 <paopau.games@gmail.com>
2024-10-21 12:08:23 +0300

    update

--- sweet_market_place14_app/models.py
+++ sweet_market_place14_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal=2)
+    image = models.ImageField(upload_to='product_images/', null=True)
+    ingredients = models.TextField()
+    weight_grams = models.PositiveIntegerField(default=0)
+    available_quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
--- sweet_market_place14_app/urls.py
+++ sweet_market_place14_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ProiecteDjango.sweet_market_place14.sweet_market_place14_app.views import home_view
+from .views import home_view
 
 app_name = 'sweet_market_place14_app'
 
